c03fc4ad7e5cf8a1ccb0ebc0235ed56b5960b0bf
资深小白程序猿 <zsxb-2468@qq.com>
2022-11-08 14:05:22 +0800

    s

--- docs/_settings.js
+++ docs/_settings.js
@@ -4,7 +4,7 @@ window.$docsify = {
     loadSidebar: true,
     subMaxLevel: 2,
     requestHeaders: {
-        'cache-control': 'max-age=0, must-revalidate',
+        'cache-control': 'no-store',
     },
     autoHeader: true,
     notFoundPage: true,
