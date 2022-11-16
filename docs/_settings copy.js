f4bf03f255d655f49937c16dfbd2f2817003e399
资深小白程序猿 <zsxb-2468@qq.com>
2022-11-17 01:34:07 +0800

    20221117

--- docs/_settings copy.js
+++ docs/_settings copy.js
@@ -1,8 +1,8 @@
 window.$docsify = {
-    name: 'HOMEWORKS',
-    // repo: 'ZSXB2468/notes_docsify',
+    name: 'ZSXB2468\'s notes',
+    repo: 'ZSXB2468/notes_docsify',
     loadSidebar: true,
-    // subMaxLevel: 2,
+    subMaxLevel: 2,
     requestHeaders: {
         'cache-control': 'no-store',
     },
